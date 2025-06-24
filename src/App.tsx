b570c6a7bb6af5eaa5a7a3725f6f26c506fa3287
Juan Vargas <juandeveloper19@gmail.com>
2025-06-24 08:59:41 -0500

    feat: Implementación de temas oscuros y claros en la aplicación
    Se han realizado cambios significativos en la estructura de estilos y componentes de la aplicación, incluyendo:
    - Adición de soporte para temas oscuros y claros mediante nuevos archivos SCSS y mixins.
    - Actualización del componente principal para permitir el cambio de tema.
    - Creación de variables y mixins para estilos globales y animaciones.
    - Eliminación del archivo .gitkeep en el directorio de estilos.
    
    Estos cambios mejoran la personalización y la experiencia del usuario al interactuar con la aplicación.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { useState } from 'react';
+import './styles/main.scss';
+
 function App() {
+ const [theme, setTheme] = useState('dark');
+
+ const handleThemeChange = (theme: string) => {
+  setTheme(theme);
+ };
+
  return (
-  <div>
-   <h1>Sea Tools</h1>
-  </div>
+  <main className={`main-content ${theme}-theme`}>
+   <div className='theme-switcher'>
+    <button className='btn-outline' onClick={() => handleThemeChange('dark')}>
+     Tema Oscuro
+    </button>
+    <button className='btn-primary' onClick={() => handleThemeChange('light')}>
+     Tema Claro
+    </button>
+   </div>
+
+   <h1>Título Principal de la Aplicación</h1>
+   <h2>Esto es un subtítulo para una sección</h2>
+   <p>
+    Este es un párrafo de texto que describe alguna característica o sección de la página. Sirve para ver cómo contrasta el texto normal con el fondo
+    y los títulos.
+   </p>
+  </main>
  );
 }
 
