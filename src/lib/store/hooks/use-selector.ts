d0790252ad51e3e99387e83275d8c5ec68a15a47
Juan Vargas <juandeveloper19@gmail.com>
2025-06-25 22:36:43 -0500

    feat: Actualizar configuración de TypeScript y Vite
    Se ha modificado el archivo `tsconfig.app.json` para extender la configuración base y se han añadido opciones de resolución de rutas en `tsconfig.json`. Además, se ha actualizado `vite.config.ts` para incluir un alias para la carpeta `src`, facilitando la importación de módulos. También se han corregido las rutas de importación en los hooks personalizados `useDispatchAppStore` y `useSelectorAppStore` para utilizar el nuevo alias.

--- src/lib/store/hooks/use-selector.ts
+++ src/lib/store/hooks/use-selector.ts
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
-import type { RootState } from '../app-store';
+import type { RootState } from '@/lib/store';
 
 //*> Hook personalizado para seleccionar datos del estado global con tipado estricto
 export const useSelectorAppStore: TypedUseSelectorHook<RootState> = useSelector;
